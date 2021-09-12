1f4c3570601e3cfbc60ea3db0fa2b487e7502cc4
lioshounet <1226741725@qq.com>
2021-09-12 16:48:24 +0800

    个人界面布局完成

--- src/components/home/myinfo.vue
+++ src/components/home/myinfo.vue
@@ -5,11 +5,13 @@
       <el-input placeholder="请输入内容" v-model="input1">
         <template slot="prepend">昵称</template>
       </el-input>
+      <br />
       <el-input placeholder="请输入内容" v-model="input2">
-        <template slot="prepend">Http://</template>
+        <template slot="prepend">电话</template>
       </el-input>
+      <br />
       <el-input placeholder="请输入内容" v-model="input3">
-        <template slot="prepend">Http://</template>
+        <template slot="prepend">邮箱</template>
       </el-input>
     </div>
     <div class="bu_div">提交按钮</div>
