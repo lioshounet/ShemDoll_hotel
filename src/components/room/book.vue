107b8765b5df2a6331554036831a259903aff22e
lioshounet <1226741725@qq.com>
2021-09-25 19:59:41 +0800

    点击商品房间卡片后可弹出选择界面，按钮功能没写

--- src/components/room/book.vue
+++ src/components/room/book.vue
@@ -4,11 +4,38 @@
       <el-card class="box-card" v-for="card in roomlist">
         <div slot="header" class="clearfix">
           <span>{{ card.hotlename }}</span>
-          <el-button style="float: right; padding: 3px 0" type="text"
-            >查看详情
+          <el-button
+            style="float: right; padding: 3px 0"
+            type="text"
+            @click="dialogTableVisible = true"
+            >订购
           </el-button>
+          <el-dialog title="订单提交" :visible.sync="dialogTableVisible">
+            <!-- 弹出内容 -->
+            <el-form :model="form">
+              <el-image :src="url" :fit="fit" v-bind:src="card.link"></el-image>
+              <div class="price">单价{{ card.price }}/晚</div>
+              <div class="allprice">
+                共计{{ Number(card.price) * Number(daynub) }}
+              </div>
+              <div class="block">
+                <span class="demonstration">选择时间</span>
+                <br />
+                <el-date-picker
+                  v-model="value1"
+                  type="daterange"
+                  range-separator="至"
+                  start-placeholder="开始日期"
+                  end-placeholder="结束日期"
+                  @change="hh()"
+                >
+                </el-date-picker>
+              </div>
+              <el-button @click="logg()">添加</el-button>
+            </el-form>
+          </el-dialog>
         </div>
-        <img v-bind:src="card.link" alt="" width="200PX" />
+        <img v-bind:src="card.link" alt="" class="showpic" />
         <!-- <img src="/img/userinfo/indoors-4234071_1920.png" alt="" /> -->
         <div class="textbox">
           <div
@@ -22,12 +49,16 @@
         </div>
       </el-card>
     </div>
+    <el-backtop
+      target=".page-component__scroll .el-scrollbar__wrap"
+    ></el-backtop>
   </div>
 </template>
 <style>
 @import url("./../../../public/scss/room/book.css");
 </style>
 <script>
+var daynub = 0;
 export default {
   name: "ShemHotelMyinfo",
   data() {
@@ -38,11 +69,25 @@ export default {
     return {
       roomlist: [],
       cardmarklist: ["酒店名称", "房号", "价格", "地址"],
+
+      //-----------------------------------------
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      formLabelWidth: "120px",
+      value1: "",
+      daynub: 0,
     };
   },
 
   mounted() {},
 
-  methods: {},
+  methods: {
+    logg() {},
+    hh() {
+      this.daynub =
+        Number((this.value1[1] - this.value1[0]) / 1000 / 3600 / 24) + 1;
+      // alert(this.daynub);
+    },
+  },
 };
 </script>
